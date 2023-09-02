0fbe16773149c298e42080c151ebdf1d9f770fb8
lordhamster66 <zhaojinbiao@lordhamster.com>
2023-09-02 16:18:49 +0800

    add .gitconfig

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+ln -sv ~/dotfiles/.gitconfig ~
+
 ln -sv ~/dotfiles/alacritty/alacritty.yml ~/.config/alacritty
 ln -sv ~/dotfiles/nvim ~/.config/
 ln -sv ~/dotfiles/zellij/config.kdl ~/.config/zellij

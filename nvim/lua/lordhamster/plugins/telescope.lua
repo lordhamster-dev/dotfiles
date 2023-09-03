cf38825c1ebbded891ea3f190cd0e4fa429a319d
lordhamster66 <zhaojinbiao@lordhamster.com>
2023-09-03 19:03:55 +0800

    update nvim

--- nvim/lua/lordhamster/plugins/telescope.lua
+++ nvim/lua/lordhamster/plugins/telescope.lua
@@ -7,39 +7,33 @@ return {
 		"nvim-tree/nvim-web-devicons",
 	},
 	config = function()
-		-- import telescope plugin safely
 		local telescope = require("telescope")
-
-		-- import telescope actions safely
-		local actions_setup, actions = pcall(require, "telescope.actions")
-		if not actions_setup then
-			return
-		end
-
-		local mappings = {
-			-- insert mode
-			i = {
-				["<C-j>"] = actions.move_selection_next,
-				["<C-k>"] = actions.move_selection_previous,
-				["<C-\\>"] = actions.file_vsplit,
-			},
-			-- normal mode
-			n = {
-				["l"] = actions.toggle_selection,
-				["L"] = actions.select_all,
-				["H"] = actions.drop_all,
-				["-"] = actions.file_split,
-				["\\"] = actions.file_vsplit,
-				["<C-j>"] = actions.move_selection_next,
-				["<C-k>"] = actions.move_selection_previous,
-				["<C-q>"] = actions.smart_send_to_qflist,
-			},
-		}
+		local actions = require("telescope.actions")
 
 		-- configure telescope
-		telescope.setup({ defaults = {
-			mappings = mappings,
-		} })
+		telescope.setup({
+			defaults = {
+				mappings = {
+					-- insert mode
+					i = {
+						["<C-j>"] = actions.move_selection_next,
+						["<C-k>"] = actions.move_selection_previous,
+						["<C-\\>"] = actions.file_vsplit,
+					},
+					-- normal mode
+					n = {
+						["l"] = actions.toggle_selection,
+						["L"] = actions.select_all,
+						["H"] = actions.drop_all,
+						["-"] = actions.file_split,
+						["\\"] = actions.file_vsplit,
+						["<C-j>"] = actions.move_selection_next,
+						["<C-k>"] = actions.move_selection_previous,
+						["<C-q>"] = actions.smart_send_to_qflist,
+					},
+				},
+			},
+		})
 
 		telescope.load_extension("fzf")
 	end,

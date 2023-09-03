cc45b89b2bb37247b365ebd65d62e715d413310c
lordhamster66 <zhaojinbiao@lordhamster.com>
2023-09-03 18:24:33 +0800

    update nvim

--- nvim/lua/lordhamster/plugins/noice.lua
+++ nvim/lua/lordhamster/plugins/noice.lua
@@ -13,16 +13,5 @@ return {
 		require("notify").setup({
 			background_colour = "#000000",
 		})
-
-		require("noice").setup({
-			lsp = {
-				-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-				override = {
-					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-					["vim.lsp.util.stylize_markdown"] = true,
-					["cmp.entry.get_documentation"] = true,
-				},
-			},
-		})
 	end,
 }

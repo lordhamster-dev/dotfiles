7fa6358b1a81a94bda459900a57a7b9721514f04
lordhamster66 <zhaojinbiao@lordhamster.com>
2023-09-03 19:27:19 +0800

    update nvim

--- nvim/lua/lordhamster/plugins/lualine.lua
+++ nvim/lua/lordhamster/plugins/lualine.lua
@@ -67,7 +67,13 @@ return {
 			sections = {
 				lualine_a = { mode },
 				lualine_b = { branch, diagnostics },
-				lualine_c = {},
+				lualine_c = {
+					{
+						require("noice").api.statusline.mode.get,
+						cond = require("noice").api.statusline.mode.has,
+						color = { fg = "#ff9e64" },
+					},
+				},
 				lualine_x = { diff, spaces, "encoding", filetype },
 				lualine_y = { location },
 				lualine_z = { "progress" },

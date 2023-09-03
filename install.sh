08c145e707870c8e3127e2ecf9b8ab46a0b140fa
lordhamster66 <zhaojinbiao@lordhamster.com>
2023-09-03 14:41:48 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 ln -sv ~/dotfiles/.gitconfig ~
 
 ln -sv ~/dotfiles/alacritty/alacritty.yml ~/.config/alacritty
-ln -sv ~/dotfiles/nvim ~/.config/
-ln -sv ~/dotfiles/zellij/ ~/.config/
+ln -sv ~/dotfiles/nvim ~/.config
+ln -sv ~/dotfiles/zellij/ ~/.config
 
-ln -sv ~/dotfiles/fish/  ~/.config/
+ln -sv ~/dotfiles/zsh/.zshrc  ~
 ln -sv ~/dotfiles/starship/starship.toml ~/.config

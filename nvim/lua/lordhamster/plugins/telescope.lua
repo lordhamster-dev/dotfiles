1f2d18b191aee36a568bb7f9b1be06891a7e3a01
lordhamster66 <zhaojinbiao@lordhamster.com>
2023-09-03 19:15:23 +0800

    update nvim

--- nvim/lua/lordhamster/plugins/telescope.lua
+++ nvim/lua/lordhamster/plugins/telescope.lua
@@ -2,6 +2,7 @@ return {
 	"nvim-telescope/telescope.nvim",
 	branch = "0.1.x",
 	dependencies = {
+		"nvim-lua/plenary.nvim",
 		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
 		"nvim-tree/nvim-web-devicons",
 	},

57c231435085bfcef6d83d0de327c1d3a14713b6
lordhamster66 <zhaojinbiao@lordhamster.com>
2023-09-06 10:55:04 +0800

    update nvim

--- nvim/lua/lordhamster/plugins/lsp/formatter.lua
+++ nvim/lua/lordhamster/plugins/lsp/formatter.lua
@@ -5,6 +5,12 @@ return {
 		local util = require("formatter.util")
 		local M = {
 			filetype = {
+				c = {
+					require("formatter.filetypes.c").clangformat,
+				},
+				cpp = {
+					require("formatter.filetypes.cpp").clangformat,
+				},
 				lua = {
 					require("formatter.filetypes.lua").stylua,
 				},
@@ -42,6 +48,15 @@ return {
 				},
 				typescript = {
 					require("formatter.filetypes.typescript").prettier,
+				},
+				json = {
+					require("formatter.filetypes.json").prettier,
+				},
+				markdown = {
+					require("formatter.filetypes.markdown").prettier,
+				},
+				yaml = {
+					require("formatter.filetypes.yaml").prettier,
 				},
 				["*"] = {
 					require("formatter.filetypes.any").remove_trailing_whitespace,

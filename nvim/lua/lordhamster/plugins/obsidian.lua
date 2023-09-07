38e41317d0899ade1306a5994ad760dea5f80d39
lordhamster66 <zhaojinbiao@lordhamster.com>
2023-09-07 09:40:34 +0800

    update nvim

--- nvim/lua/lordhamster/plugins/obsidian.lua
+++ nvim/lua/lordhamster/plugins/obsidian.lua
@@ -26,6 +26,11 @@ return {
 				-- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
 				-- ["gf"] = require("obsidian.mapping").gf_passthrough(),
 			},
+			follow_url_func = function(url)
+				-- Open the URL in the default web browser.
+				vim.fn.jobstart({ "open", url }) -- Mac OS
+				-- vim.fn.jobstart({"xdg-open", url})  -- linux
+			end,
 		}
 	end,
 }

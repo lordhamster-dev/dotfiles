a74344c57328040958d01c02e961fb343a63a626
lordhamster66 <zhaojinbiao@lordhamster.com>
2023-12-28 15:57:25 +0800

    update alacritty

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 ln -sv ~/dotfiles/.gitconfig ~
 
-ln -sv ~/dotfiles/alacritty/alacritty.yml ~/.config/alacritty
+ln -sv ~/dotfiles/alacritty/alacritty.toml ~/.config/alacritty
 ln -sv ~/dotfiles/nvim ~/.config
 ln -sv ~/dotfiles/zellij/ ~/.config
 

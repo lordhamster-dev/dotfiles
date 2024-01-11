94676cdb422f1c4851c9b0ba289c90f2a7815475
lordhamster66 <zhaojinbiao@lordhamster.com>
2024-01-11 11:21:26 +0800

    update nvim

--- nvim/lua/lordhamster/core/options.lua
+++ nvim/lua/lordhamster/core/options.lua
@@ -94,3 +94,8 @@ vim.api.nvim_create_autocmd("BufReadPost", {
 --   autocmd InsertLeave * :lua vim.fn.system("im-select com.apple.keylayout.ABC")
 -- augroup END
 -- ]])
+
+vim.api.nvim_create_user_command("Rfinder", function()
+	local path = vim.api.nvim_buf_get_name(0)
+	os.execute("open -R " .. path)
+end, {})

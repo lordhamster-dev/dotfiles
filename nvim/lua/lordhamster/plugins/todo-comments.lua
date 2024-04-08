4ceb7872becb5da3da633c88d04d376f660d7ff1
lordhamster66 <zhaojinbiao@lordhamster.com>
2024-04-08 11:12:28 +0800

    update nvim add todo-comments

---
+++ nvim/lua/lordhamster/plugins/todo-comments.lua
@@ -0,0 +1,23 @@
+return {
+	"folke/todo-comments.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+	},
+	config = function()
+		local todo_comments = require("todo-comments")
+
+		-- set keymaps
+		local keymap = vim.keymap
+
+		keymap.set("n", "]t", function()
+			todo_comments.jump_next()
+		end, { desc = "Next todo comments" })
+
+		keymap.set("n", "[t", function()
+			todo_comments.jump_prev()
+		end, { desc = "Previous todo comments" })
+
+		todo_comments.setup()
+	end,
+}

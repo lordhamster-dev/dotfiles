f5eb86f259d24ac620ebb08eca684e8bab31ef50
lordhamster66 <zhaojinbiao@lordhamster.com>
2024-04-12 17:55:01 +0800

    update nvim

--- nvim/lua/lordhamster/backup/bufferline.lua
+++ nvim/lua/lordhamster/backup/bufferline.lua
@@ -1,28 +1,28 @@
 return {
-	"akinsho/bufferline.nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-	version = "*",
-	opts = {
-		options = {
-			numbers = "none", -- | "ordinal" | "buffer_id" | "both" | function({ ordinal, id, lower, raise }): string,
-			close_command = "Bdelete! %d", -- can be a string | function, see "Mouse actions"
-			right_mouse_command = "Bdelete! %d", -- can be a string | function, see "Mouse actions"
-			max_name_length = 30,
-			max_prefix_length = 30, -- prefix used when a buffer is de-duplicated
-			tab_size = 21,
-			diagnostics = false, -- | "nvim_lsp" | "coc",
-			diagnostics_update_in_insert = false,
-			offsets = { { filetype = "NvimTree", text = "File Explorer", text_align = "center", separator = true } },
-			show_buffer_icons = true,
-			show_tab_indicators = true,
-			persist_buffer_sort = false, -- whether or not custom sorted buffers should persist
-			always_show_bufferline = true,
-			hover = {
-				enabled = true,
-				delay = 200,
-				reveal = { "close" },
-			},
-			sort_by = "insert_at_end",
-		},
-	},
+  "akinsho/bufferline.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  version = "*",
+  opts = {
+    options = {
+      numbers = "none", -- | "ordinal" | "buffer_id" | "both" | function({ ordinal, id, lower, raise }): string,
+      close_command = "Bdelete! %d", -- can be a string | function, see "Mouse actions"
+      right_mouse_command = "Bdelete! %d", -- can be a string | function, see "Mouse actions"
+      max_name_length = 30,
+      max_prefix_length = 30, -- prefix used when a buffer is de-duplicated
+      tab_size = 21,
+      diagnostics = false, -- | "nvim_lsp" | "coc",
+      diagnostics_update_in_insert = false,
+      offsets = { { filetype = "NvimTree", text = "File Explorer", text_align = "center", separator = true } },
+      show_buffer_icons = true,
+      show_tab_indicators = true,
+      persist_buffer_sort = true, -- whether or not custom sorted buffers should persist
+      always_show_bufferline = true,
+      hover = {
+        enabled = true,
+        delay = 200,
+        reveal = { "close" },
+      },
+      sort_by = "insert_at_end",
+    },
+  },
 }

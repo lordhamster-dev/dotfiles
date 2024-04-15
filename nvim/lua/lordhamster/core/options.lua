c3c21d75df41d86b88ff41773b18e8a1fc90b10e
lordhamster66 <zhaojinbiao@lordhamster.com>
2024-04-15 15:00:09 +0800

    update nvim

--- nvim/lua/lordhamster/core/options.lua
+++ nvim/lua/lordhamster/core/options.lua
@@ -2,6 +2,10 @@ local opt = vim.opt -- for conciseness
 
 vim.g.maplocalleader = ";"
 
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
 -- utf8
 opt.encoding = "UTF-8"
 opt.fileencoding = "utf-8"

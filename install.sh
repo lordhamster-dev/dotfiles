0af1fc27fe1db4499b94f84e771253157a955412
lordhamster66 <zhaojinbiao@lordhamster.com>
2024-04-16 14:07:47 +0800

    update tmux conf

--- install.sh
+++ install.sh
@@ -6,5 +6,5 @@ ln -sv ~/dotfiles/zellij/ ~/.config
 
 ln -sv ~/dotfiles/zsh/.zshrc  ~
 ln -sv ~/dotfiles/starship/starship.toml ~/.config
-ln -sv ~/dotfiles/.tmux.conf ~
+ln -sv ~/dotfiles/tmux/.tmux.conf ~
 ln -sv ~/dotfiles/karabiner/complex_modifications/* ~/.config/karabiner/assets/complex_modifications

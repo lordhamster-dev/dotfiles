74b5c70b671f2014d7e9c31ad454d679f43ef5f6
lordhamster66 <zhaojinbiao@lordhamster.com>
2024-05-07 10:56:53 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,6 @@ ln -sv ~/dotfiles/.gitconfig ~
 
 ln -sv ~/dotfiles/alacritty/alacritty.toml ~/.config/alacritty
 ln -sv ~/dotfiles/nvim ~/.config
-ln -sv ~/dotfiles/zellij/ ~/.config
 
 ln -sv ~/dotfiles/zsh/.zshrc  ~
 ln -sv ~/dotfiles/starship/starship.toml ~/.config

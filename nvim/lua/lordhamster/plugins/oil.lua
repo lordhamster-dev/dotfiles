52f8a696a7ae3341ccf5898183186b48d701dc90
lordhamster66 <zhaojinbiao@lordhamster.com>
2024-05-22 16:25:46 +0800

    update nvim oil plugin

--- nvim/lua/lordhamster/plugins/oil.lua
+++ nvim/lua/lordhamster/plugins/oil.lua
@@ -2,7 +2,18 @@
 return {
   -- https://github.com/stevearc/oil.nvim
   "stevearc/oil.nvim",
-  opts = {},
   -- Optional dependencies
   dependencies = { "nvim-tree/nvim-web-devicons" },
+  opts = {
+    keymaps = {
+      ["<C-s>"] = false,
+      ["<C-h>"] = false,
+      ["<C-\\>"] = "actions.select_vsplit",
+      ["_"] = false,
+      ["<Tab>"] = "actions.open_cwd",
+    },
+    view_options = {
+      show_hidden = true,
+    },
+  },
 }

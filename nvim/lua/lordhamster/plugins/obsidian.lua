1e9ac3199fb570aff67082a61a61299c87616c40
lordhamster66 <zhaojinbiao@lordhamster.com>
2024-05-23 21:42:01 +0800

    update nvim obsidian plugin conf

--- nvim/lua/lordhamster/plugins/obsidian.lua
+++ nvim/lua/lordhamster/plugins/obsidian.lua
@@ -76,8 +76,8 @@ return {
           -- [">"] = { char = "", hl_group = "ObsidianRightArrow" },
           -- ["~"] = { char = "󰰱", hl_group = "ObsidianTilde" },
           -- Replace the above with this if you don't have a patched font:
-          [" "] = { char = "󰄱", hl_group = "ObsidianTodo" },
-          ["x"] = { char = "✔", hl_group = "ObsidianDone" },
+          [" "] = { char = "", hl_group = "ObsidianTodo" },
+          ["x"] = { char = "󰸞", hl_group = "ObsidianDone" },
           -- You can also add more custom ones...
         },
       },

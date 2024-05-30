6932a1a4416d86b37a0d05a95cc06d10167f7328
lordhamster66 <zhaojinbiao@lordhamster.com>
2024-05-30 10:20:47 +0800

    update hammerspoon conf for auto change input method

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,3 +1,7 @@
+------------------------------------------------------------------------------------------------
+---------   Don't use local or it will goes out of scope and gets garbage collected.   ---------
+------------------------------------------------------------------------------------------------
+
 -- -- 自动重新加载config
 function reloadConfig(files)
 	local doReload = false
@@ -10,7 +14,9 @@ function reloadConfig(files)
 		hs.reload()
 	end
 end
-hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
+
+hsConfigWatcher = hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig)
+hsConfigWatcher:start()
 
 function searchFromClipboard(url)
 	-- 获取剪贴板内容
@@ -54,6 +60,46 @@ end)
 --
 -- -- 启动 Caffeine.spoon 并设置默认状态
 -- spoon.Caffeine:start()
+
+-- 当选中某窗口按下 ctrl+command+. 时会显示应用的路径、名字等信息
+hs.hotkey.bind({ "ctrl", "cmd" }, ".", function()
+	hs.pasteboard.setContents(hs.window.focusedWindow():application():path())
+	hs.alert.show(
+		"App path:        "
+			.. hs.window.focusedWindow():application():path()
+			.. "\n"
+			.. "App name:      "
+			.. hs.window.focusedWindow():application():name()
+			.. "\n"
+			.. "IM source id:  "
+			.. hs.keycodes.currentSourceID(),
+		hs.alert.defaultStyle,
+		hs.screen.mainScreen(),
+		3
+	)
+end)
+
+-- 这里指定中文和英文输入法的 ID
+function Chinese()
+	hs.keycodes.currentSourceID("com.sogou.inputmethod.sogou.pinyin")
+end
+function English()
+	hs.keycodes.currentSourceID("com.apple.keylayout.ABC")
+end
+
+-- activated 时切换到指定的输入法，deactivated 时恢复之前的状态
+function applicationWatcher(appName, eventType, _)
+	if eventType == hs.application.watcher.activated then
+		if appName == "Alacritty" then
+			English()
+		else
+			Chinese()
+		end
+	end
+end
+
+appWatcher = hs.application.watcher.new(applicationWatcher)
+appWatcher:start()
 
 -- 显示通知以确认配置已加载
 hs.notify.new({ title = "Hammerspoon", informativeText = "Configuration Loaded" }):send()

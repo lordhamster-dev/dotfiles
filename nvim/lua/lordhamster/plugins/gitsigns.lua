16979f5d4f1727385d025770c48fba46fbf1708a
lordhamster66 <zhaojinbiao@lordhamster.com>
2024-06-04 13:09:51 +0800

    update nvim add vim-fugitive

--- nvim/lua/lordhamster/plugins/gitsigns.lua
+++ nvim/lua/lordhamster/plugins/gitsigns.lua
@@ -3,6 +3,9 @@ return {
   -- https://github.com/lewis6991/gitsigns.nvim
   "lewis6991/gitsigns.nvim",
   event = { "BufReadPre", "BufNewFile" },
+  dependencies = {
+    "tpope/vim-fugitive",
+  },
   config = function()
     -- https://github.com/lewis6991/gitsigns.nvim
     require("gitsigns").setup({

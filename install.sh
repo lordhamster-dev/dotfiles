183d643ff21c099ca88b82274cf2b228b67b12f8
lordhamster66 <zhaojinbiao@lordhamster.com>
2024-06-07 14:02:56 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,11 @@
 ln -sv ~/dotfiles/clash/lordhamster.yaml ~/.config/clash
-
-ln -sv ~/dotfiles/.gitconfig ~
-
-ln -sv ~/dotfiles/alacritty/alacritty.toml ~/.config/alacritty
-ln -sv ~/dotfiles/wezterm  ~/.config
-ln -sv ~/dotfiles/nvim ~/.config
-
 ln -sv ~/dotfiles/zsh/.zshrc  ~
 ln -sv ~/dotfiles/starship/starship.toml ~/.config
 ln -sv ~/dotfiles/tmux/.tmux.conf ~
+ln -sv ~/dotfiles/.gitconfig ~
+
+ln -sv ~/dotfiles/alacritty/alacritty.toml ~/.config/alacritty
+ln -sv ~/dotfiles/nvim ~/.config
+
 ln -sv ~/dotfiles/karabiner/complex_modifications/* ~/.config/karabiner/assets/complex_modifications
 ln -sv ~/dotfiles/hammerspoon  ~/.hammerspoon

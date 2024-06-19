5e49891411344403631201ddda16f42d2e24024e
lordhamster66 <zhaojinbiao@lordhamster.com>
2024-06-19 17:54:11 +0800

    update nvim

--- nvim/lua/lordhamster/plugins/noice.lua
+++ nvim/lua/lordhamster/plugins/noice.lua
@@ -14,6 +14,8 @@ return {
   config = function()
     require("notify").setup({
       background_colour = "#000000",
+      top_down = false,
+      render = "compact",
     })
 
     require("noice").setup({

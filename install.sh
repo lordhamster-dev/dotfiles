2fc9976dc6bf00f1e899d404a91b274c6fd7ce94
lordhamster66 <zhaojinbiao@lordhamster.com>
2024-07-02 20:26:28 +0800

    archive some stuff

--- install.sh
+++ install.sh
@@ -3,10 +3,7 @@ ln -sv ~/dotfiles/zsh/.zshrc  ~
 ln -sv ~/dotfiles/starship/starship.toml ~/.config
 ln -sv ~/dotfiles/tmux/.tmux.conf ~
 ln -sv ~/dotfiles/.gitconfig ~
-
-ln -sv ~/dotfiles/alacritty/alacritty.toml ~/.config/alacritty
 ln -sv ~/dotfiles/kitty   ~/.config
 ln -sv ~/dotfiles/nvim ~/.config
-
-ln -sv ~/dotfiles/karabiner/complex_modifications/* ~/.config/karabiner/assets/complex_modifications
 ln -sv ~/dotfiles/hammerspoon  ~/.hammerspoon
+ln -sv ~/dotfiles/karabiner/complex_modifications/* ~/.config/karabiner/assets/complex_modifications

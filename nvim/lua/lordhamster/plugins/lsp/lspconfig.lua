8e687c6159413a8e61fc2edfa9f7eb2765fa7d17
lordhamster66 <zhaojinbiao@lordhamster.com>
2024-07-04 18:05:37 +0800

    update nvim lspconfig conf

--- nvim/lua/lordhamster/plugins/lsp/lspconfig.lua
+++ nvim/lua/lordhamster/plugins/lsp/lspconfig.lua
@@ -102,8 +102,14 @@ return {
         lspconfig["pyright"].setup({
           capabilities = capabilities,
           settings = {
+            pyright = {
+              disableOrganizeImports = true,
+            },
             python = {
-              analysis = {},
+              analysis = {
+                -- Ignore all files for analysis to exclusively use Ruff for linting
+                ignore = { "*" },
+              },
             },
           },
         })

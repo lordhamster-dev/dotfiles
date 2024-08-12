967d770776e0f3d6921a00b408434df39a293196
lordhamster66 <zhaojinbiao@lordhamster.com>
2024-08-12 13:47:44 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ ln -sv ~/dotfiles/starship/starship.toml ~/.config
 ln -sv ~/dotfiles/tmux ~/.config
 ln -sv ~/dotfiles/.gitconfig ~
 ln -sv ~/dotfiles/kitty   ~/.config
+ln -sv ~/dotfiles/aerospace   ~/.config
 ln -sv ~/dotfiles/nvim ~/.config
 ln -sv ~/dotfiles/yazi  ~/.config
 ln -sv ~/dotfiles/hammerspoon  ~/.hammerspoon

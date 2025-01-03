e65ad3d3143a5bfee1c58101f0f302dbe7b37611
lordhamster-dev <zhaojinbiao@lordhamster.com>
2025-01-03 15:55:51 +0800

    update nvim lualine conf

--- nvim/lua/lordhamster/plugins/lualine.lua
+++ nvim/lua/lordhamster/plugins/lualine.lua
@@ -149,16 +149,7 @@ return {
             color = { fg = "#ff9e64" },
           },
         },
-        lualine_x = {
-          {
-            require("noice").api.status.message.get_hl,
-            cond = require("noice").api.status.message.has,
-          },
-          diff,
-          spaces,
-          "encoding",
-          filetype,
-        },
+        lualine_x = { diff, spaces, "encoding", filetype },
         lualine_y = { location },
         lualine_z = { progress },
       },

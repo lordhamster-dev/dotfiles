e5054c60bd8a81df9af321db07d33eb034aecf15
lordhamster-dev <zhaojinbiao@lordhamster.com>
2025-02-07 19:49:11 +0800

    update avante.lua

--- nvim/lua/lordhamster/plugins/ai/avante.lua
+++ nvim/lua/lordhamster/plugins/ai/avante.lua
@@ -6,10 +6,10 @@ return {
   version = false, -- set this if you want to always pull the latest change
   opts = {
     provider = "openai",
-    auto_suggestions_provider = "copilot", -- Since auto-suggestions are a high-frequency operation and therefore expensive, it is recommended to specify an inexpensive provider or even a free provider: copilot
+    auto_suggestions_provider = "openai", -- Since auto-suggestions are a high-frequency operation and therefore expensive, it is recommended to specify an inexpensive provider or even a free provider: copilot
     openai = {
-      endpoint = "https://api.deepseek.com/v1",
-      model = "deepseek-chat",
+      endpoint = "https://api.siliconflow.cn/v1",
+      model = "Pro/deepseek-ai/DeepSeek-V3",
       temperature = 0,
       max_tokens = 4096,
     },

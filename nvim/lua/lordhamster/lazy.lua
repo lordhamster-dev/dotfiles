bc7f59b512895b6b02e087a79e632b4e0a595c6d
lordhamster-dev <zhaojinbiao@lordhamster.com>
2025-02-20 14:25:18 +0800

    update nvim

--- nvim/lua/lordhamster/lazy.lua
+++ nvim/lua/lordhamster/lazy.lua
@@ -11,15 +11,16 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup(
-  { { import = "lordhamster.plugins" }, { import = "lordhamster.plugins.lsp" }, { import = "lordhamster.plugins.ai" } },
-  {
-    checker = {
-      enabled = true,
-      notify = false,
-    },
-    change_detection = {
-      notify = false,
-    },
-  }
-)
+require("lazy").setup({
+  { import = "lordhamster.plugins" },
+  { import = "lordhamster.plugins.lsp" },
+  { import = "lordhamster.plugins.ai" },
+}, {
+  checker = {
+    enabled = true,
+    notify = false,
+  },
+  change_detection = {
+    notify = false,
+  },
+})

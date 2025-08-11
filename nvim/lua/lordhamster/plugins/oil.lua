e028d1ff4756f0bfac379a439b772fd22a182411
lordhamster-dev <zhaojinbiao@lordhamster.com>
2025-08-11 13:51:41 +0800

    feat(oil): disable trash for file deletion

--- nvim/lua/lordhamster/plugins/oil.lua
+++ nvim/lua/lordhamster/plugins/oil.lua
@@ -14,7 +14,7 @@ return {
       ["l"] = "actions.select",
     },
     -- Send deleted files to the trash instead of permanently deleting them (:help oil-trash)
-    delete_to_trash = true,
+    delete_to_trash = false,
     -- Skip the confirmation popup for simple operations (:help oil.skip_confirm_for_simple_edits)
     skip_confirm_for_simple_edits = true,
     view_options = {

8ea2122478ca97aeb2e6fb7474444c5dd55528bf
lordhamster-dev <zhaojinbiao@lordhamster.com>
2025-12-11 15:06:24 +0800

    chore(plugins): upgrade rustaceanvim to ^6 and enable cargo.allFeatures

--- nvim/lua/lordhamster/plugins/lsp/rustaceanvim.lua
+++ nvim/lua/lordhamster/plugins/lsp/rustaceanvim.lua
@@ -1,6 +1,11 @@
 return {
   -- https://github.com/mrcjkb/rustaceanvim
   "mrcjkb/rustaceanvim",
-  version = "^5", -- Recommended
+  version = "^6", -- Recommended
   lazy = false, -- This plugin is already lazy
+  ["rust-analyzer"] = {
+    cargo = {
+      allFeatures = true,
+    },
+  },
 }
